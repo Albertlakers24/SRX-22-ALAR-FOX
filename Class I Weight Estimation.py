3913ebbf9c5bf0eb48314a1514904579a8878653
Albertlakers24 <albertchou2000@hotmail.com>
2022-11-29 17:07:08 +0100

    Changes to Class I Weight Estimation

--- Class I Weight Estimation.py
+++ Class I Weight Estimation.py
@@ -3,7 +3,10 @@ import numpy as np
 # Outputs: MTOW OEW and WF
 # Relationships: MTOW = WOE + WF + WPL
 # Relationships: MTOW = WE + WF + WPLtot + Wtfo
-
+# LOOK INTO MF/MTO FORMULA FROM ROLOEF READER
+# LIQUID FUELS WILL HAVE THE SAME FORMULA AS JET-A FUEL
+# BATTERIES WILL HAVE FORMULA WITHOUT ln()
+#
 
 #Constants
 g = 9.80665
