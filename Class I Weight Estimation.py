17314b4dca47d59534f655076655ec66fcb62bcf
Albertlakers24 <albertchou2000@hotmail.com>
2022-11-29 15:36:25 +0100

    Changes to Class I Weight Estimation

--- Class I Weight Estimation.py
+++ Class I Weight Estimation.py
@@ -6,12 +6,14 @@ import numpy as np
 # Relationship: Wtfo = Mtfo * Wto
 # Relationship: WOE = a * MTOW + b
 # Cd = 2*Cd0
-# Cl = np.sqrt(np.pi()*A*Cd0 * A)
+# Cl = np.sqrt(np.pi()*e*Cd0 * A)
 #
 a = 0.5422 or 0.4985 #Linear regression from MTOW/ OEW (turboprop or turbrojet)
 b = 1455.2 or 1782.3 #Linear regression from MTOW/ OEW (turboprop or turbrojet)
 g = 9.80665
 A = #Aspect ratio (CHANGE)
+e = #Oswald factor
 Cd0 = #zero lift drag (CHANGE)
+Cl = np.sqrt(np.pi()*Cd0*A*e)
 R = 1000 * 1852 # range in meters
 E = 35 * 60 # loiter endurance in seconds
