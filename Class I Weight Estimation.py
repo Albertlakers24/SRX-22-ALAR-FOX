11ca57dff061f323af4e32270b5a3befe41a40e4
Albertlakers24 <albertchou2000@hotmail.com>
2022-11-29 14:35:45 +0100

    Changes to Class I Weight Estimation

--- Class I Weight Estimation.py
+++ Class I Weight Estimation.py
@@ -3,4 +3,6 @@
 # Relationships: MTOW = WOE + WF + WPL
 # Relationship: WOE = WE + Wtfo + Wcrew
 # Relationship: Wtfo = Mtfo * Wto
-
+# Relationship: WOE = a * MTOW + b
+a = input("What is the a value of the linear regression?")
+b = input("What is the b value of the linear regression?")
