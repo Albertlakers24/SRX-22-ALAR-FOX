29283b4f12600e42df951db2659982233964dd9b
ElisabethInes <74304402+ElisabethInes@users.noreply.github.com>
2022-11-30 10:53:53 +0100

    Range

--- Class I Weight Estimation.py
+++ Class I Weight Estimation.py
@@ -53,18 +53,35 @@ CD = 2 * Cd0
 R_lost = 1 / 0.7 * (CL/CD) *(h_cruise + (V_cruise**2 / (2*g)))
 Req = (R_norm + R_lost)*(1+f_con) + 1.2 * R_div + E*V_cruise
 
-fuelfractioncruise_propeller = eta_p/(g*c_p)
-fuelfractioncruise_jet = V_cruise/(g*c_j)
+#take-off, climb, descent, deceleration
+R = R_lost*(1+f_con)
+mfuel_MTO = 1- np.exp(-R/(eta_eng*eta_p*(e_f/g)*(CL/CD)))
+mbat_MTO = R/(eta_EM*eta_p*(e_f/g)*(CL/CD))
 
-W4_5 = np.exp(R*(CD/CL)*(1/fraction_propeller))     # W4/W5 for propeller
-W4_5 = np.exp(R*(CD/CL)*(1/fraction_jet))           # W4/W5 for jet
+#cruise + loiter cruise
+R = (R_norm)*(1+f_con) + 1.2 * R_div + E*V_cruise
+mfuel_MTO = 1- np.exp(-R/(eta_eng*eta_p*(e_f/g)*(CL/CD)))
+mbat_MTO = R/(eta_EM*eta_p*(e_f/g)*(CL/CD))
+
+#cruise + loiter cruise + take-off, clmib, descent, deceleration
+R = Req
+mfuel_MTO = 1- np.exp(-R/(eta_eng*eta_p*(e_f/g)*(CL/CD)))
+mbat_MTO = R/(eta_EM*eta_p*(e_f/g)*(CL/CD))
+
+
+
+#fuelfractioncruise_propeller = eta_p/(g*c_p)
+#fuelfractioncruise_jet = V_cruise/(g*c_j)
+
+#W4_5 = np.exp(R*(CD/CL)*(1/fraction_propeller))     # W4/W5 for propeller
+#W4_5 = np.exp(R*(CD/CL)*(1/fraction_jet))           # W4/W5 for jet
 
 #W8/W9
-fuelfractionloiter_propeller = eta_p/(V*g*c_p)      
-fuelfractionloiter_jet = 1/(g*c_j)
+#fuelfractionloiter_propeller = eta_p/(V*g*c_p)
+#fuelfractionloiter_jet = 1/(g*c_j)
 
-W8_9 = np.exp(E*(1/fuelfractionloiter_propeller)*(CD/CL))   # W8/W9 for propeller
-W8_9 = np.exp(E*(1/fuelfractionloiter_jet)*(CD/CL))   # W8/W9 for jet
+#W8_9 = np.exp(E*(1/fuelfractionloiter_propeller)*(CD/CL))   # W8/W9 for propeller
+#W8_9 = np.exp(E*(1/fuelfractionloiter_jet)*(CD/CL))   # W8/W9 for jet
 
 
 W1_TO =                      #statistics dependent on aircraft
