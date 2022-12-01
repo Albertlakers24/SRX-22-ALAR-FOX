2d20e43913a85f7d81fdfe8232f40101030c2b9a
ElisabethInes <74304402+ElisabethInes@users.noreply.github.com>
2022-12-01 13:01:52 +0100

    Range

--- Class I Weight Estimation.py
+++ Class I Weight Estimation.py
@@ -28,9 +28,8 @@ e_bat = 1.1                     #MJ/kg Specific Energy Battery (assuming 300Wh/k
 PAX = 50                        #Number of passengers
 WCargo =                        #Cargo Weight (TBD)
 eta_EM = 0.95                   # Electric motor efficiency
-PSFC =                          # Power Specific Fuel Consumption
-TSFC =                          # Thrust Specific Fuel Consumption
-eta_p_home = 0.7                # Propulsive efficiency - homebuilts
+PSFC = 0.48                     # Power Specific Fuel Consumption    --- to be checked
+TSFC = 0.67                     # Thrust Specific Fuel Consumption
 eta_p_single = 0.80             # Propulsive efficiency - single engine/motor props
 eta_p_twin = 0.82               # Propulsive efficiency - twin engine props
 eta_p_turbo = 0.85              # Propulsive efficiency - regional turboprops
@@ -40,23 +39,27 @@ b_p = 1439                      # turboprop
 b_j = 1782.3                    # turbojet
 
 # Variable Constants depending on aircraft features
-A =                             # Aspect ratio (CHANGE) - high A for slender wing
+A =    #(CHANGE)                # Aspect ratio - high A for slender wing
+Cfe =                           #Equivalent skin friction coefficient - depending on aircraft from empirical estimation
+Swet_S =                        # Wetted area ratios - depending on airframe structure
+
+# Depending on energy source
+e_f =                           # Specfic Energy per fuel type  (TYPES: kerosene, SAF, LH)
+eta_eng =                       # Engine efficiency             (TYPES: jet, propeller)
+eta_p =                         # Propulsive efficiency depending on airplane types (TYPES: single, twin, regional turboprop)
+a =                             # Linear regression for OEW     (TYPES: turboprop, turbojet)
+b =                             # Linear regression for OEW     (TYPES: turboprop, turbojet)
+
 #Cdo calculations
 Psi = 0.0075 #Parasite drag dependent on the lift coefficient (value based on Roelof reader p.46)
 phi = 0.97   #span efficiency factor (value based on Roelof reader p.46)
 e = 1/((np.pi)*A*Psi+(1/phi))
-Cfe =                           #Equivalent skin friction coefficient - depending on aircraft from empirical estimation
-Swet_S =                        #Wetted area ratios - depending on airframe structure
+
 Cd0 = Cfe * Swet_S
 CL = np.sqrt(np.pi()*Cd0*A*e)
 CD = 2 * Cd0
 
-# Depending on energy source
-e_f =                           # Specfic Energy per fuel type
-eta_eng =                       # Engine efficiency
-eta_p =                         # Propulsive efficiency depending on airplane types
-a =                             #Linear regression for OEW
-b =                             #Linear regression for OEW
+
 
 #calculation Turboprop/piston
 eta_eng = (1/e_f)*(1/PSFC)      # Thermodynamic efficiency of engine
@@ -67,6 +70,8 @@ eta_eng = (V_cruise/TSFC)*(1/e_f)*(1/eta_p)
 R_lost = 1 / 0.7 * (CL/CD) *(h_cruise + (V_cruise**2 / (2*g)))
 Req = (R_norm + R_lost)*(1+f_con) + 1.2 * R_div + E*V_cruise
 
+
+
 #take-off, climb, descent, deceleration
 R = R_lost*(1+f_con)
 mfuel_MTO = 1- np.exp(-R/(eta_eng*eta_p*(e_f/g)*(CL/CD)))
@@ -76,6 +81,8 @@ mbat_MTO = R/(eta_EM*eta_p*(e_bat/g)*(CL/CD))
 R = (R_norm)*(1+f_con) + 1.2 * R_div + E*V_cruise
 mfuel_MTO = 1- np.exp(-R/(eta_eng*eta_p*(e_f/g)*(CL/CD)))
 mbat_MTO = R/(eta_EM*eta_p*(e_bat/g)*(CL/CD))
+
+
 
 #cruise + loiter cruise + take-off, clmib, descent, deceleration
 R = Req
@@ -95,4 +102,6 @@ MTOW = (b + WPLtot)/(Mff-a)
 WOE = a*MTOW + b
 WF = (Mres+1)*(1-Mff)*MTOW                  ##TO BE CHECKED
 
-##REWRITTEN main FORMULA & add output formulas
+print("MTOW =", MTOW)
+print("Fuel weight =", WF)
+print("Operational Empty Weight =", WOE)
