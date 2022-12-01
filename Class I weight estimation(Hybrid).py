1df07bb69e6fc7e639da23e4cd274f589dc2ba90
Albertlakers24 <albertchou2000@hotmail.com>
2022-12-01 12:34:33 +0100

    Add to Class I Weight Estimation (Hybrid)

--- Class I weight estimation(Hybrid).py
+++ Class I weight estimation(Hybrid).py
@@ -12,3 +12,20 @@ e_kero = 42.9                   #MJ/kg Specific Energy Kerosene
 e_atj = 43.2                    #MJ/kg Specific Energy SAF(ATJ)
 e_lh2 = 142                     #MJ/kg Specific Energy Liquid Hydrogen
 e_bat = 1.1                     #MJ/kg Specific Energy Battery (assuming 300Wh/kg)
+V_stall =
+#Cdo calculations
+Psi = 0.0075 #Parasite drag dependent on the lift coefficient (value based on Roelof reader p.46)
+phi = 0.97   #span efficiency factor (value based on Roelof reader p.46)
+e = 1/((np.pi)*A*Psi+(1/phi))
+Cfe =                           #Equivalent skin friction coefficient - depending on aircraft from empirical estimation
+Swet_S =                        #Wetted area ratios - depending on airframe structure
+Cd0 = Cfe * Swet_S
+CL = np.sqrt(np.pi()*Cd0*A*e)
+CD = 2 * Cd0
+#Aerodynamic Estimations
+CL_to =
+CD_to =
+CL_0 =
+CL_max =
+# Stall Speed Requirement
+W_S = 1/2 * rho * V_stall **2 * CL_max
