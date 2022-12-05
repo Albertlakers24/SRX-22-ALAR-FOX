a0114e79090570412721e9907e5adbc439041dfd
thokke <thokke@student.tudelft.nl>
2022-12-05 14:28:31 +0100

    Added ISA, CL and CD calculations

--- Lift_Drag_Polar.py
+++ Lift_Drag_Polar.py
@@ -1,26 +1,38 @@
 import numpy as np
-# import class i weight estimation
+from Class_I_Weight_Estimation import Cd0, A, e, MTOW
 
-S_w = 5
-Surfaces_ratio = 6
-C_fe = 0.5
-e = 0.3
-rho_sea = 1.225 #kg/m3
-rho_cruise = 0.02047 * rho_sea
-V_cruise = 275 #KTAS
-h_cruise = 28000 #ft
-ft_to_m = 0.3048
+#Constants
+g_0 = 9.80665
+Molar_mass_air = 0.0289644 #kg/mol
+universal_gas_constant = 8.31432 #N m kmol⁻¹ K⁻¹
+specific_gas_constant = 287.052 #J·kg⁻¹·K⁻¹
 
-C_D_0 = Surfaces_ratio * C_fe
+b = 25 #wingspan in m
+S_w = b**2 / A
+kts_ms = 0.514444444
+V_cruise = 275 * kts_ms #KTAS
+T_0 = 288.15
+p_0 = 101325 #Pa
+lapse_rate = -0.0065
+
+
+def ISA_calculator(h):
+    T = T_0 + lapse_rate * h
+    p = p_0 * ((T_0 / T) ** ((g_0 * Molar_mass_air) / (universal_gas_constant * lapse_rate)))
+    rho = p / (specific_gas_constant * T)
+    return T, p, rho
+
+T, p, rho = ISA_calculator(28000 * 0.3048)
 
 def C_L_calc(W, rho, V):
     C_L_calc = 2 * W / (rho * V**2 * S_w)
     return C_L_calc
 
-def C_D_calc(AR, W, rho, V):
+def C_D_calc(AR, W, rho, V, C_D_0):
     C_D = C_D_0 + C_L_calc(W, rho, V)**2 / (np.pi * e * AR)
-    return C_D
+    return C_D, rho
 
-print(C_D_calc(5, 23405, 1.0, 300))
+print(C_D_calc(A, MTOW, rho, V_cruise, Cd0))
+print(C_L_calc(MTOW, rho, V_cruise))
 
 
