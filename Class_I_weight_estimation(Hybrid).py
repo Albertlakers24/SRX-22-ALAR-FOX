75ae097f72c90f0fb2e36877a7291b8d74e2f2b8
Albertlakers24 <albertchou2000@hotmail.com>
2022-12-06 11:35:25 +0100

    Tuesday Changes

--- Class_I_weight_estimation(Hybrid).py
+++ Class_I_weight_estimation(Hybrid).py
@@ -35,7 +35,9 @@ CL_land =  2.6                  #Change with Estimate (1.9-3.3)
 TOP = 430                       #Change with Literature Reference to slide (420-460) -> from Raymer graph
 ROC = 10.2                      #Change with CS25 and literature or Requirement (Rate of Climb)
 ROC_V = 0.0032                  #Change with CS25 and literature or Requirement (Climb Gradient) ROC/V
-V_stall = 100                      #Change with CS25 or Requirement
+MTOW_design=
+V_stall = np.sqrt(1/2 * rho_cruise * CL_max
+V_stall = 45                      #Change with CS25 or Requirement
 
 #CALCULATIONS for the GRAPHS
 W_P_TOP = TOP/ (W_S) * CL_to * rho_1524_rho0 #(Use this if it's at a different altitude then sea level)
@@ -48,7 +50,7 @@ W_P_ROC = eff_prop / (ROC + ((np.sqrt(W_S)*np.sqrt(2/rho_1524))/(1.345*((A*e)**(
 # Climb Gradent Constraint
 W_P_CV = eff_prop / (np.sqrt(W_S)*(ROC_V + CD_to/CL_to)*(np.sqrt((2/rho_1524)*(1/CL_to))))
 #Stall Constraint
-W_S_stall = 1/2 * rho_0 * V_stall**2 * CL_max
+W_S_stall = 1/2 * rho_cruise * V_stall**2 * CL_max
 
 plt.vlines(W_S_stall,0,0.4,'b',label="V_stall")
 plt.plot(W_S,W_P_TOP,'r',label = "Takeoff Constraint")
@@ -67,7 +69,6 @@ plt.grid()
 plt.show()
 
 '''#Mass Preliminary Calculation
-MTOW_design=
 W_P_design =
 W_S_design =
 P_max = MTOW_design / W_P_design
