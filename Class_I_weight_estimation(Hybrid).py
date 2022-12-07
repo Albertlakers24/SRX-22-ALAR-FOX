3e4e0d93a823bdf69fac79b66487bf75f82214c1
Albertlakers24 <albertchou2000@hotmail.com>
2022-12-07 12:01:33 +0100

    Tuesday Changes

--- Class_I_weight_estimation(Hybrid).py
+++ Class_I_weight_estimation(Hybrid).py
@@ -36,7 +36,7 @@ CL_land = 2.6                      #Change with Estimate (1.9-3.3)
 TOP = 430                          #Change with Literature Reference to slide (420-460) -> from Raymer graph
 ROC = 10.2                         #Change with CS25 and literature or Requirement (Rate of Climb)
 ROC_V = 0.0024                     #Change with CS25 and literature or Requirement (Climb Gradient) ROC/V
-V_stall = 54                       #Change with CS25 or Requirement
+V_stall = 52                       #Change with CS25 or Requirement
 a = 0.5464
 b = 1439*g
 
@@ -53,7 +53,7 @@ W_P_CV = eff_prop / (np.sqrt(W_S)*(ROC_V + (CD_to/CL_to))*(np.sqrt((2/rho_1524)*
 #Stall Constraint
 W_S_stall = 1/2 * rho_1524 * V_stall**2 * CL_to
 
-'''plt.vlines(W_S_stall,0,100,'b',label="V_stall")
+plt.vlines(W_S_stall,0,100,'b',label="V_stall")
 plt.plot(W_S,W_P_TOP,'r',label = "Takeoff Constraint")
 plt.vlines(W_S_land,0,100,'k',label ="Landing")
 plt.plot(W_S,W_P_cru,'m',label = "Cruise Constraint")
@@ -67,18 +67,19 @@ plt.xlabel("W/S (N/m^2)")
 plt.ylabel("W/P (N/W)")
 plt.legend(loc = "upper right")
 plt.grid()
-plt.show()'''
+plt.show()
 
 #Mass Preliminary Calculation
-W_P_design = 0.0468
-W_S_design = 3112
+W_P_design = 0.0597
+W_S_design = 2886
 m_turboprop = 1074.5
 m_em = 50
+m_propeller = 17
 MTOW_design = 207146                    #N
 P_max = MTOW_design / W_P_design
 S = MTOW_design / W_S_design
-#print(P_max/1000, "kW Max Power")
-#print(S,"m^2 Surface Area ")
+print(P_max/1000, "kW Max Power")
+print(S,"m^2 Surface Area ")
 #Degree of Hybridization of Energy (He) *Could be defined by each split point or total journey
 t_cruise_500 = 4476
 t_cruise_full = 17568
@@ -115,33 +116,33 @@ E_nc = 0 * E_total_full
 He = E_nc / E_total_full        #Energy of non consumable(battery) / Total Energy
 #print(E_nc/10**6,"MJ for non consumable")
 #print((E_total_full - E_nc)/10**6,"MJ for consumable")
-eta_stt = 0.85 * 0.45       #Efficiency chain from shaft-to-thrust
-eta_fuel_cell = 0.7 * 0.95 * 0.99
-eta_btt = 0.95 * 0.75       #Efficiency chain from battery-to-thrust
+eta_stt = 0.75 * 0.45       #Efficiency chain from shaft-to-thrust
+eta_fuel_cell = 0.75 * 0.95 #Efficiency chain from shaft-to-thrust (fuel cell)
+eta_btt = 0.95 * 0.75        #Efficiency chain from battery-to-thrust
 NoD_ice = 2                 #Number of turboprop engines
-NoD_em = 4                 #Number of electric motor engines
-P_ice = (E_total_full - E_nc)/ (eta_stt * t_total_full * NoD_ice)
-P_em = E_nc/ (eta_btt * t_total_full * NoD_em)
-P_ice_climb1 = (E_total_climb1)/ (eta_fuel_cell * t_climb1 * NoD_ice)
-P_ice_climb2 = (E_total_climb2)/ (eta_fuel_cell * t_climb2 * NoD_ice)
-P_ice_climb3 = (E_total_climb3)/ (eta_fuel_cell * t_climb3 * NoD_ice)
-P_ice_cruise_500 = (E_total_cruise_500)/ (eta_fuel_cell * t_cruise_500 * NoD_ice)
-P_ice_cruise_full = (E_total_cruise_full)/ (eta_fuel_cell * t_cruise_full * NoD_ice)
+NoD_em = 6                 #Number of electric motor engines
+#P_ice = (E_total_full - E_nc)/ (eta_stt * t_total_full * NoD_ice)
+#P_em = E_nc/ (eta_btt * t_total_full * NoD_em)
+P_ice_climb1 = (E_total_climb1)/ (eta_stt * t_climb1 * NoD_ice)
+P_ice_climb2 = (E_total_climb2)/ (eta_stt * t_climb2 * NoD_ice)
+P_ice_climb3 = (E_total_climb3)/ (eta_stt * t_climb3 * NoD_ice)
+P_ice_cruise_500 = (E_total_cruise_500)/ (eta_stt * t_cruise_500 * NoD_ice)
+P_ice_cruise_full = (E_total_cruise_full)/ (eta_stt * t_cruise_full * NoD_ice)
 print(np.round(P_ice_climb1/10**3), "kW Climb1")
 print(np.round(P_ice_climb2/10**3), "kW Climb2")
 print(np.round(P_ice_climb3/10**3), "kW Climb3")
 print(np.round(P_ice_cruise_500/10**3), "kW Cruise 500nmi")
 print(np.round(P_ice_cruise_full/10**3), "kW Cruise full")
-
-#print(P_ice/1000,"kW for ICE per engine")
-#print(P_em/1000,"kW for Electric Battery per engine")
+'''
+print(P_ice/1000,"kW for ICE per engine")
+print(P_em/1000,"kW for Electric Battery per engine")
 #Degree of Hybridization of Power(Hp)
 # Choice between Parallel and Series needs to be made
 #If Parallel:
 H_p_para = P_em*NoD_em / P_max
-#print(np.round(H_p_para,2)*100,"% Hybridlization")
-'''#If Series:
-H_p_ser = P_em_max / P_ice_max'''
+print(np.round(H_p_para,2)*100,"% Hybridlization")
+#If Series:
+#H_p_ser = P_em_max / P_ice_max
 tf =  0                      #Trap fuel time step
 BSFC= 0.48*(0.45/(745*3600)) #Brake-specific fuel consumption
 ddp = 0.8                   #Deep discharge protection
@@ -149,11 +150,12 @@ E_bat = 2.7*10**6           #Total Battery Energy per piece
 m_fuel_ice = (1+tf)*P_ice*NoD_ice*BSFC*t_total_500
 m_bat = (1+ddp) * (E_nc/(eta_btt*E_bat))
 m_OE = (a * MTOW_design + b)/g - m_turboprop        #Maximum Takeoff Mass
-m_propulsion = m_turboprop + m_em*NoD_em
+m_propulsion = m_turboprop + (m_em + m_propeller)*NoD_em
 m_MTOW = m_OE + m_fuel_ice + m_payload + m_bat + m_propulsion
-'''print(m_fuel_ice, "Fuel Mass(kg)")
+print(m_fuel_ice, "Fuel Mass(kg)")
 print(m_bat,"Battery Mass(kg)")
 print(m_OE, "Operational Empty mass without Engine(kg)")
 print(m_propulsion, "Propulsion Mass (kg)")
 print(m_MTOW, "Calculated MTOW (kg)")
-print(MTOW_design/g, "MTOW Design(kg)")'''
+print(MTOW_design/g, "MTOW Design(kg)")
+print(np.round(m_MTOW - MTOW_design/g,4), "Difference between hybrid and original design")'''
