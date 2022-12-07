a27135358c700e2fd50b523b818f3bbbc9a7e82c
gsa <gcjosa@outlook.com>
2022-12-07 19:03:24 +0100

    commit

--- Fuselage.py
+++ Fuselage.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy as sp
+#Switch
+switch = 1          # put 1 for circular fuselage, 2 for double bubble fuselage
 
 # Some constants
 k_cabin = 1.08      #For single aisle
@@ -11,16 +13,50 @@ l_lav = 0.914
 w_lav = 0.914
 l_galley = 0.762
 w_galley = 0.914
+seat_pitch = 1
 
 # Design choices
-n_SA = 3            # Number of seats abreast
-n_PAX = 48          # Number of passengers (46-50)
+n_SA = 5            # Number of seats abreast
+
+if n_SA == 3:
+    n_PAX = 48          # Number of passengers (46-50)
+    D_inner = 2.48      # inner diameter of the fuselage cross section [m]
+elif n_SA == 4:
+    n_PAX = 48          # Number of passengers (46-50)
+    D_inner = 2.8       # inner diameter of the fuselage cross section [m]
+elif n_SA == 5:
+    n_PAX = 50          # Number of passengers (46-50)
+    D_inner = 3.5       # inner diameter of the fuselage cross section [m]
 n_row = np.ceil(n_PAX/n_SA)
+l_cp = 4            # cockpit length [m]
+double_bubble_height = 2.7
 
+if switch == 1:
+    height = D_inner
+else:
+    height = double_bubble_height
 
+# Basic Length/Dimensions calculations
+D_eff = np.sqrt(height*D_inner)             # effective (inner) diameter [m]
+l_t = 1.6 * D_eff                           # Length of tailcone [m]
+l_cabin = n_row * k_cabin                   # Length of the cabin [m]
+skin_thickness = 0.084 + 0.045* D_eff       # fuselage skin thickness [m]
+D_outer = D_eff - skin_thickness            # outer fuselage diameter [m]
 
+fuel_at_belly = False
 
+if fuel_at_belly == True:
+    l_tank = 0
+else:
+    l_tank = 4
+l_f = l_cabin + l_t + l_cp                  # length of the fuselage [m]
+fineness_ratio = l_f/D_eff
 
-# Outputs
-D_eff = np.sqrt(width*height)
-
+#printing results
+print("N_row:", n_row)
+print("N_SA:", n_SA)
+print("D_in [m]:", D_inner)
+print("D_out [m]:", D_outer)
+print("skin thickness [m]:", skin_thickness)
+print("l_fuselage [m]:", l_f)
+print("Fineness Ratio: ", fineness_ratio)
