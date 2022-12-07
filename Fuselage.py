c7ab55469119c98998d6910420c19d5aea9073f8
gsa <gcjosa@outlook.com>
2022-12-07 21:08:15 +0100

    commit

--- Fuselage.py
+++ Fuselage.py
@@ -16,19 +16,19 @@ w_galley = 0.914
 seat_pitch = 1
 
 # Design choices
-n_SA = 5            # Number of seats abreast
+n_SA = 4           # Number of seats abreast
 
 if n_SA == 3:
-    n_PAX = 48          # Number of passengers (46-50)
-    D_inner = 2.48      # inner diameter of the fuselage cross section [m]
+    n_PAX = 48                  # Number of passengers (46-50)
+    D_inner = 2.48              # inner diameter of the fuselage cross section [m]
 elif n_SA == 4:
-    n_PAX = 48          # Number of passengers (46-50)
-    D_inner = 2.8       # inner diameter of the fuselage cross section [m]
+    n_PAX = 48                  # Number of passengers (46-50)
+    D_inner = 2.8               # inner diameter of the fuselage cross section [m]
 elif n_SA == 5:
-    n_PAX = 50          # Number of passengers (46-50)
-    D_inner = 3.5       # inner diameter of the fuselage cross section [m]
-n_row = np.ceil(n_PAX/n_SA)
-l_cp = 4            # cockpit length [m]
+    n_PAX = 50                  # Number of passengers (46-50)
+    D_inner = 3.5               # inner diameter of the fuselage cross section [m]
+n_row = np.ceil(n_PAX/n_SA)     # number of rows
+l_cp = 4                        # cockpit length [m]
 double_bubble_height = 2.7
 
 if switch == 1:
@@ -38,19 +38,28 @@ else:
 
 # Basic Length/Dimensions calculations
 D_eff = np.sqrt(height*D_inner)             # effective (inner) diameter [m]
-l_t = 1.6 * D_eff                           # Length of tailcone [m]
+l_t = 1.6 * D_eff                           # Length of tail [m]
 l_cabin = n_row * k_cabin                   # Length of the cabin [m]
-skin_thickness = 0.084 + 0.045* D_eff       # fuselage skin thickness [m]
-D_outer = D_eff - skin_thickness            # outer fuselage diameter [m]
+skin_thickness = 0.084 + 0.045 * D_eff      # fuselage skin thickness [m]
+D_outer = D_eff + skin_thickness            # outer fuselage diameter [m]
 
-fuel_at_belly = False
+fuel_at_belly = True
 
 if fuel_at_belly == True:
     l_tank = 0
 else:
-    l_tank = 4
-l_f = l_cabin + l_t + l_cp                  # length of the fuselage [m]
-fineness_ratio = l_f/D_eff
+    l_tank = 4                                      # Length of the fuel tank as fuselage section
+l_f = l_cabin + l_t + l_cp + l_tank                 # length of the fuselage [m]
+fineness_ratio = l_f/D_eff                          # Fineness ratio
+l_nc = 2 * D_eff                                    # length of the nose cone [m]
+l_tc = 3 * D_eff                                    # length of the tail cone
+# Skin Drag Computation
+
+
+
+# Fuselage mass computation
+
+
 
 #printing results
 print("N_row:", n_row)
@@ -58,5 +67,7 @@ print("N_SA:", n_SA)
 print("D_in [m]:", D_inner)
 print("D_out [m]:", D_outer)
 print("skin thickness [m]:", skin_thickness)
+print("L_tank [m]:", l_tank)
 print("l_fuselage [m]:", l_f)
 print("Fineness Ratio: ", fineness_ratio)
+
