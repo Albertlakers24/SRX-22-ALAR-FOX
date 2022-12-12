c8866e6b0631fc022d41f5fedc652796b18ee2dc
Albertlakers24 <albertchou2000@hotmail.com>
2022-12-12 16:03:49 +0100

    Add Separate Wing Loading Diagram file

--- Wing Loading Diagram.py
+++ Wing Loading Diagram.py
@@ -33,7 +33,7 @@ CL_max = 1.9                       #(1.2-1.8 twin engine) & (1.5-1.9 turboprop) 
 CL_to = 2.1                        #Change with Estimate (1.7-2.1)
 CD_to = Cd0 + (CL_to**2 /(np.pi * A* e))
 CL_land = 2.6                      #Change with Estimate (1.9-3.3)
-CD_land = Cd0 + (CL_to**2 /(np.pi * A* e))
+CD_land = Cd0 + (CL_land**2 /(np.pi * A* e))
 TOP = 430                          #Change with Literature Reference to slide (420-460) -> from Raymer graph
 ROC = 6.9                         #Change with CS25 and literature or Requirement (Rate of Climb)
 ROC_V = 0.0024                     #Change with CS25 and literature or Requirement (Climb Gradient) ROC/V
@@ -42,22 +42,22 @@ a = 0.5088
 b = 1199.7
 
 #CALCULATIONS for the GRAPHS
-W_P_TOP = TOP/ (W_S) * CL_to #* rho_1524_rho0 #(Use this if it's at a different altitude then sea level)
+W_P_TOP = TOP/ (W_S) * CL_to * rho_1524_rho0 #(Use this if it's at a different altitude then sea level)
 # Landing Distance Constraint
-W_S_land = (CL_land * rho_1524 * s_landing_1524/0.5847)/(2*0.95)
+W_S_land = (CL_land * rho_1524 * s_landing_1524/0.5847)/(2*0.98)
 # Cruise Speed Constraint
 W_P_cru = eff_prop * (rho_cruise_rho0)**(3/4) * ((((Cd0*1/2*rho_cruise*V_cruise**3)/W_S)+(W_S/(np.pi*A*e*1/2*rho_cruise*V_cruise)))**(-1))
 # Rate of Climb Constraint
-W_P_ROC = eff_prop / (ROC + ((np.sqrt(W_S)*np.sqrt(2/rho_0))/(1.345*((A*e)**(3/4))/(Cd0**(1/4)))))
-# Climb Gradent Constraint
-W_P_CV = eff_prop / (np.sqrt(W_S)*(ROC_V + (CD_to/CL_to))*(np.sqrt((2/rho_0)*(1/CL_to))))
-#Stall Constraint
+W_P_ROC = eff_prop / (ROC + ((np.sqrt(W_S)*np.sqrt(2/rho_1524))/(1.345*((A*e)**(3/4))/(Cd0**(1/4)))))
+# Climb Gradient Constraint
+W_P_CV = eff_prop / (np.sqrt(W_S)*(ROC_V + (CD_to/CL_to))*(np.sqrt((2/rho_1524)*(1/CL_to))))
+#Approach Constraint
 W_S_approach = 1/2 * rho_1524 * V_approach**2 * CL_land
 
 plt.vlines(W_S_approach,0,100,'b',label="Approach Speed Constraint")
 plt.plot(W_S,W_P_TOP,'r',label = "Takeoff Constraint")
 plt.vlines(W_S_land,0,100,'k',label ="Landing Constraint")
-plt.axvspan(3271,4000,color = "red", alpha = 0.1)
+plt.axvspan(3171,4000,color = "red", alpha = 0.1)
 plt.plot(W_S,W_P_cru,'m',label = "Cruise Constraint")
 plt.fill_between(W_S,W_P_cru,1,color = "red",alpha = 0.1)
 plt.plot(W_S,W_P_ROC,'c',label = "Rate of Climb Constraint")
@@ -73,7 +73,9 @@ plt.grid()
 plt.show()
 
 #Mass Preliminary Calculation
-W_P_design = 0.04706
-W_S_design = 3271
+W_P_design = 0.0467
+W_S_design = 3171
 m_turboprop = 1074.5/2
 MTOW_design = 20281 * g                  #N
+print(MTOW_design/W_S_design,"m^2")
+print(MTOW_design/W_P_design/10**3,"kW")
