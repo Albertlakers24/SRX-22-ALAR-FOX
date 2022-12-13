940e40ea6dfffd9c8e53cb42a901c57ced56f94a
ElisabethInes <74304402+ElisabethInes@users.noreply.github.com>
2022-12-13 09:09:01 +0100

    Version Tuesday

--- Empennage Design.py
+++ Empennage Design.py
@@ -2,42 +2,80 @@ import numpy as np
 from Class_I_Weight_Estimation import V_cruise, MTOW
 from Lift_Drag_Polar import b
 from Wing_planform import Sw, c_mac, specific_gas_constant, T, p, gamma, M_cross
+from Fuselage import D_outer, l_f
+
+
+##Design 1; Liquid Hydrogen + Fuel cell, Propeller on main wing and small EP on wing tip, tank aft
+##Design 2; Liquid Hydrogen, propeller on main wing, tank aft
+##Design 3; EM SP, Small propellers on wing tip, engines aft, fuel tanks in wing
+##Design 4; EHS, small propellers on wing tip, engines aft, fuel in wing and aft
+Design = 2
 
 #Constants from other files
 cw = c_mac                          # m c_mac from Wing_planform
 bw = b                              # m b from Lift_Drag_Polar
-gamma = 1.4                         # Specific heat ratio of gas
-M_cross = 0.935                     # Technology factor for supercritical airfoils
 
-#CHOOSE
-lh = 24                             # m from CG calculations I think
-lv = 24                             # m from CG calculations
-Vh = 0.68113                        # TBD horizontal tail volume from data  -> 0.68113 (propeller)
-Vv = 0.08                           # TBD vertical tail volume from data    -> 0.08 (expected for propeller)
-sweeph_quarter = 0                  # degrees (0 for propeller)
-sweepv_LE = 25                      # degrees (0-50) propeller
-Av = 1.3                            # - (1-2)
-Ah = 4                              # - (3-5)
-taperv = 0.5                        # - (0.3-0.7) taper ratio
-taperh = 0.45                       # - (0.3-1) taper ratio
-sweepv_half = 25                    # degrees -> depending on the t/c
-fraction_df = 0.19                  # -
-sweep_df_LE = 74                    # degrees TBD
+
+
+if Design ==1:
+    Vh = 0.9  # TBD horizontal tail volume from data  -> 0.68113 (propeller)
+    Vv = 0.08  # TBD vertical tail volume from data    -> 0.08 (expected for propeller)
+    Av = 1  # - (1-2)
+    Ah = 4  # - (3-5)
+    taperv = 0.5  # - (0.3-0.7) taper ratio
+    taperh = 0.8  # - (0.3-1) taper ratio
+    Kc = 1.4
+
+    l_opt = 1.4 * np.sqrt((4 * c_mac * Sw * Vh) / (np.pi * D_outer))
+if Design ==2:
+    Vh = 0.9  # TBD horizontal tail volume from data  -> 0.68113 (propeller)
+    Vv = 0.08  # TBD vertical tail volume from data    -> 0.08 (expected for propeller)
+    Av = 1  # - (1-2)
+    Ah = 4  # - (3-5)
+    taperv = 0.5  # - (0.3-0.7) taper ratio
+    taperh = 0.3  # - (0.3-1) taper ratio
+    Kc = 1.4
+
+    l_opt = 1.4 * np.sqrt((4 * c_mac * Sw * Vh) / (np.pi * D_outer))
+if Design ==3:
+    Vh = 0.9  # TBD horizontal tail volume from data  -> 0.68113 (propeller)
+    Vv = 0.08  # TBD vertical tail volume from data    -> 0.08 (expected for propeller)
+    Av = 1  # - (1-2)
+    Ah = 4  # - (3-5)
+    taperv = 0.5  # - (0.3-0.7) taper ratio
+    taperh = 0.8  # - (0.3-1) taper ratio
+    Kc = 1.4
+
+    l_opt = 1.4 * np.sqrt((4 * c_mac * Sw * Vh) / (np.pi * D_outer))
+if Design ==4:
+    Vh = 0.9  # TBD horizontal tail volume from data  -> 0.68113 (propeller)
+    Vv = 0.08  # TBD vertical tail volume from data    -> 0.08 (expected for propeller)
+    Av = 1  # - (1-2)
+    Ah = 4  # - (3-5)
+    taperv = 0.5  # - (0.3-0.7) taper ratio
+    taperh = 0.8  # - (0.3-1) taper ratio
+    Kc = 1.4
+
+    l_opt = 1.4 * np.sqrt((4 * c_mac * Sw * Vh) / (np.pi * D_outer))
+else:
+    print("Hello!??")
+
 
 #Check with graph
-Sh_stability = 1.06                 # Determine from the stability graph
+Sh_stability = 11.7                 # Determine from the stability graph
 
-#Intermediate Equations --now for the wing, to be changed?!!!!!!!
-l_opt =
-
+#Intermediate Equations
+print("l_opt=", l_opt)
+print("l_f=", l_f)
 
 ####HORIZONTAL TAIL
 #Calculations changes for Conventional =1, Conventional plus dorsal =2
-Type = 1
+switch =1
+Type =1
 
 if Type ==1:
-    Sh = Vh * (Sw * cw) / lh                # m^2 Horizontal tail surface area
-    if abs((Sh-Sh_stability)/Sh)*100 < 10:
+    Sh = Vh * (Sw * cw) / l_opt                # m^2 Horizontal tail surface area
+    if switch ==1:         #abs((Sh-Sh_stability)/Sh)*100 < 10:
         #Optimum Tail moment arm
 
         #Determine span, root chord, tipchord MAC
@@ -50,11 +88,59 @@ if Type ==1:
         #Position the surface
 
         #Print Statements
+        print("-----------Horizontal Tail Sizing------------")
         print("Sh =", Sh, "m^2")
-        print("")
+        print("bh =", bh, "m")
+        print("cr_h =", c_rh, "m")
+        print("ct_h =",c_th, "m")
+        print("c_mach =", c_mach_h)
+        print("y_machh =", y_mach_h)
+
+        Sv = Vv * (Sw * bw) / l_opt
+        bv = np.sqrt(Av*Sv)
+        c_rv = (2+Sv)/((1+taperv)*bv)
+        c_tv = taperv*c_rv
+        c_mac_v =(2/3)*c_rv*((1+taperv+taperv**2)/(taperv+1))
+        y_mach_v = 0.5 * (1 / 3) * (1 + 2 * taperv) / (1 + taperv) * bv
+        print("-----------Vertical Tail Sizing------------")
+        print("Sv =", Sv, "m^2")
+        print("bv =", bv, "m")
+        print("cr_v =", c_rv, "m")
+        print("ct_v =",c_tv, "m")
+        print("c_macv =", c_mac_v)
+        print("y_machv =", y_mach_v)
+
     else:
         print("Sh =", Sh, "m^2")
         print("Rerun Code for lh")
+
+
+print("----------------DRAWING CALCULATIONS-------------")
+xcg = 13
+
+print("Location Tail =", xcg + l_opt)
+print("Distance from TE to tail =", l_f-(xcg+l_opt))
+#print("distance outside fuselage=", l_f-(xcg+l_opt+c_rh))
+
+
+
+
+
+
+
+#CHOOSE
+#lh = 24                            # m from CG calculations I think
+#lv = 24                            # m from CG calculations
+#sweep_df_LE = 74                    # degrees TBD
+#sweeph_quarter = 0                  # degrees (0 for propeller)
+#sweepv_LE = 25                      # degrees (0-50) propeller
+#sweepv_half = 25                    # degrees -> depending on the t/c
+#fraction_df = 0.19                  # -
+
+
+
+
+
 
 ''''
 if Type ==2:
@@ -102,4 +188,4 @@ if Type ==1:
 if Type ==2:
     print("goodbye")
 if Type ==3:
-    print("H")''''
+    print("H")'''
