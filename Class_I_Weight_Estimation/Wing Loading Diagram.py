8da29f84d82a2cd53a055f4c93313c03b99696cd
Albertlakers24 <albertchou2000@hotmail.com>
2022-12-22 11:37:44 +0100

    new constants

--- Class_I_Weight_Estimation/Wing Loading Diagram.py
+++ Class_I_Weight_Estimation/Wing Loading Diagram.py
@@ -168,7 +168,7 @@ W_S_land = s_land_constraint(s_landing_1524,C_LFL,rho_0,CL_land,beta_em)
 W_P_cru = cruise_contraint(eff_prop,1,0.026,1.009,70,W_S,9,0.71,beta_em)
 W_P_ROC = roc_constraint(0.8,1,2.0,0.026,1.23,9.0,0.71,W_S,1,1,2)
 W_P_CV = climb_gradient_constraint(0.8,1,0.083,0.16,1.5,1.23,W_S,1,1,2)
-W_P_TOP = takeoff_constraint(1,750,1.23,50*ft,k_t,1,2)'''
+W_P_TOP = takeoff_constraint(1,750,1.23,50*ft,k_t,1,2)
 plt.vlines(W_S_approach,0,100,'b',label="Approach Speed Constraint")
 plt.plot(W_S,W_P_TOP,'g',label = "Takeoff Constraint")
 plt.plot(W_S,W_P_TOP_OEI,'r',label = "Takeoff Constraint (OEI)")
@@ -219,12 +219,14 @@ elif propulsion_type ==4:
 plt.legend(loc = "upper right")
 plt.grid()
 plt.show()
-
-print("Hydrogen Combustion W/S = 3552")
+'''
+'''print("Hydrogen Combustion W/S = 3552")
 print("Hydrogen Combustion W/P =  0.0535")
 print("Fuel Cell W/S = 3560")
 print("Fuel Cell W/P = 0.0763 ")
 print("Hybrid Series W/S = 3564")
 print("Hybrid Series W/P =  0.0764")
 print("Hybrid Parallel Series W/S = 3554")
-print("Hybrid Parallel Series W/P = 0.07")
+print("Hybrid Parallel Series W/P = 0.07")'''
+W_P_design = 0.0763
+W_S_design = 3560
