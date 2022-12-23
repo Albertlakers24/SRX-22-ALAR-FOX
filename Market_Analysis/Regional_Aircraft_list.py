a288dc213faf48a181c1993f792285399487b30a
Albertlakers24 <albertchou2000@hotmail.com>
2022-12-23 14:44:58 +0100

    Hub Determination

---
+++ Market_Analysis/Regional_Aircraft_list.py
@@ -0,0 +1,5 @@
+import pandas as pd
+aircraft = pd.read_csv('aircrafts.txt',names = ["Name","ICAO","IATA","Capacity","Country"],delimiter=';',na_values="-").dropna()
+del aircraft['Country']
+aircraft = aircraft[aircraft['Capacity'] < 100]
+aircraft = aircraft[aircraft['Capacity'] > 20]
