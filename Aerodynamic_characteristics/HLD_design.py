5ec31211f9e06cb9d90ae5f23b7adbdd62ea2943
Albertlakers24 <albertchou2000@hotmail.com>
2023-01-09 14:59:11 +0100

    HLD Setup

--- Aerodynamic_characteristics/HLD_design.py
+++ Aerodynamic_characteristics/HLD_design.py
@@ -6,9 +6,21 @@ def HLD_TE_deltaClmax(Cf,df,flap_type):
     if flap_type == "single slotted":
         delta_c =       #Torenbeek page 533
         c_prime_over_c = 1 + delta_c*Cf
-        delta_clmax = 1.3
+        delta_clmax_TE = 1.3
     if flap_type == "double slotted":
-
+        if df < 15.01:
+            delta_c = 0.3/15                                                    #Torenbeek page 533
+        else:
+            delta_c = 0.3 + df * ((0.70 - 0.30) / (60 - 15))                    #Torenbeek page 533
+        c_prime_over_c = 1 + delta_c * Cf
+        delta_clmax_TE = 1.3 * c_prime_over_c
     if flap_type == "fowler":
+        if df < 10.01:
+            delta_c = 0.45 / (1/1.2 * 10)                                       #Torenbeek page 533
+        else:
+            delta_c = 0.45 + df * ((0.65 - 0.45 )/(45 - (1/1.2*10)))            #Torenbeek page 533
+        c_prime_over_c = 1 + delta_c*Cf
+        delta_clmax_TE = 1.3 * c_prime_over_c
 
+    return delta_clmax_TE,c_prime_over_c
 def HLD_TE_deltaClmax(Cf, df, flap_type):
