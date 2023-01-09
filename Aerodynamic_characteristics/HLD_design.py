c672c73341c917124ebab2b335dcf7f5e0e6e7ef
Albertlakers24 <albertchou2000@hotmail.com>
2023-01-09 11:54:52 +0100

    Changes for HLD Designs

--- Aerodynamic_characteristics/HLD_design.py
+++ Aerodynamic_characteristics/HLD_design.py
@@ -2,4 +2,13 @@ import numpy as np
 from Constants import *
 from Class_I_Weight_Estimation.Class_I_weight_estimation_Fuelcell_FINAL import m_mto
 from Initial_Aircraft_Sizing.Wing_planform import Sw
-CL_max_required = (m_mto * g) / (1/2 * ISA_calculator(takeoff_critical,0)[2]*Sw*V_stall)
+def HLD_TE_deltaClmax(Cf,df,flap_type):
+    if flap_type == "single slotted":
+        delta_c =       #Torenbeek page 533
+        c_prime_over_c = 1 + delta_c*Cf
+        delta_clmax = 1.3
+    if flap_type == "double slotted":
+
+    if flap_type == "fowler":
+
+def HLD_TE_deltaClmax(Cf, df, flap_type):
